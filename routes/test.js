87ea26d3ba493fdee20171204450f36d430004a6
Grand Bistro <gopichand0215@gmail.com>
2025-06-07 20:57:52 +0530

    updatedddd

--- routes/test.js
+++ routes/test.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res, next) => {
+  console.log("✅ Backend deployed successfully at", new Date());
   res.status(201).json({
     status: true,
     message: "Welcome to cyt test test",
