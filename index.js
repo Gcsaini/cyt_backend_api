60c7cdb50654259c6acb48f82c622911d2b64439
Grand Bistro <gopichand0215@gmail.com>
2025-06-11 12:38:00 +0530

    updated code

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ connect(process.env.MONGODB_URI)
   .then(() => {
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
-      console.log(`✅ Server running on port ${PORT}`);
+      console.log(`✅ Server running on port new ${PORT}`);
     });
   })
   .catch((err) => console.log(err));
