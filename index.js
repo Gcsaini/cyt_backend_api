3fcdfbdf7e0c8a6f7463968402df64202b339371
Grand Bistro <gopichand0215@gmail.com>
2025-06-12 19:02:35 +0530

    updat conde

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ mongoose.set("strictQuery", true);
 
 connect(process.env.MONGODB_URI)
   .then(() => {
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 4000;
     app.listen(PORT, () => {
       console.log(`✅ Server running on port new ${PORT}`);
     });
